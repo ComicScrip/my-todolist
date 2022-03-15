f1e1a3701985978adae5307b703a5b2f325dc314
Pierre Genthon <pierre.genthon@wildcodeschool.com>
2022-03-15 12:35:07 +0100

    ability to add a task to the table

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const newTaskForm = document.getElementById('newTaskForm');
+const newTaskInput = document.getElementById('newTaskInput');
+const tasksTable = document.getElementById('taskTable');
+
+console.log(newTaskForm, newTaskInput);
+
+newTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const newTaskName = newTaskInput.value;
+
+  const line = document.createElement('tr');
+  const titleElement = document.createElement('td');
+  titleElement.innerHTML = newTaskName;
+  line.appendChild(titleElement);
+  tasksTable.appendChild(line);
+  newTaskInput.value = '';
+});
